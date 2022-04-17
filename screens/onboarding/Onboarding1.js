127e6d90f586d506573b80da247b5fae600ec56e
andikscript <andikscript@gmail.com>
2022-04-17 22:22:57 +0700

    add onboarding1

--- screens/onboarding/Onboarding1.js
+++ screens/onboarding/Onboarding1.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Image, StatusBar, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { COLORS } from '../../constants';
+import * as Progress from 'react-native-progress';
 
 const Onboarding1 = () => {
   return (
-    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+    <View style={styles.container}>
       <StatusBar
         backgroundColor={'transparent'}
         barStyle={'light-content'}
@@ -17,68 +18,95 @@ const Onboarding1 = () => {
         angle={90}
         angleCenter={{x: 0.5, y: 0.5}}
         colors={[COLORS.blueyoung, COLORS.purpleyoung]}
-        style={{
-          flex: 1,
-          borderBottomLeftRadius: 190,
-          borderBottomRightRadius: 280,
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-        }}>
+        style={styles.boxImage}>
         <Image
           source={require('../../assets/image/onboarding1.png')}
-          style={{
-            width: 200,
-            height: 300,
-            marginBottom: 1,
-          }}
+          style={styles.image}
         />
       </LinearGradient>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: COLORS.white,
-        }}>
-        <Text
-          style={{
-            fontSize: 22,
-            fontFamily: 'Poppins-Bold',
-            color: COLORS.black,
-            marginTop: 20,
-            marginLeft: 20,
-          }}>
-          Track Your Goal
-        </Text>
-        <Text
-          style={{
-            fontSize: 14,
-            fontFamily: 'Poppins-Light',
-            color: COLORS.black,
-            marginHorizontal: 20,
-            marginTop: 5,
-            textAlign: 'justify',
-          }}>
+      <View style={styles.boxText}>
+        <Text style={styles.heading}>Track Your Goal</Text>
+        <Text style={styles.desc}>
           Don't worry if you have trouble determining your goals, We can help
           you determine your goals and track your goals.
         </Text>
       </View>
-      <TouchableOpacity style={{position: 'absolute', bottom: 20, right: 20}}>
+      <View style={styles.progress}>
+        <Progress.Circle
+          size={75}
+          progress={0.25}
+          borderColor="transparent"
+          endAngle={0.5}
+          indeterminate={false}
+          color={COLORS.purpleyoung}
+        />
+      </View>
+      <TouchableOpacity style={styles.button}>
         <LinearGradient
           useAngle={true}
           angle={90}
           angleCenter={{x: 0.5, y: 0.5}}
           colors={[COLORS.blueyoung, COLORS.purpleyoung]}
-          style={{
-            width: 60,
-            height: 60,
-            borderRadius: 50,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-          <Icon name='chevron-right' size={30} color={'white'} />
+          style={styles.gradient}>
+          <Icon name="chevron-right" size={30} color={'white'} />
         </LinearGradient>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  boxImage: {
+    flex: 1,
+    borderBottomLeftRadius: 190,
+    borderBottomRightRadius: 280,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 300,
+    marginBottom: 1,
+  },
+  boxText: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  heading: {
+    fontSize: 22,
+    fontFamily: 'Poppins-Bold',
+    color: COLORS.black,
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Light',
+    color: COLORS.black,
+    marginHorizontal: 20,
+    marginTop: 5,
+  },
+  progress: {
+    position: 'absolute',
+    bottom: 12.5,
+    right: 12.5,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  gradient: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Onboarding1;
