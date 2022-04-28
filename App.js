db27dcb93693bc53a8370eac676bac53a74db8ea
andikscript <andikscript@gmail.com>
2022-04-28 14:17:58 +0700

    unit test

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { View } from 'react-native';
 import { WelcomeScreen1 } from './screens/onboarding';
 import Onboarding1 from './screens/onboarding/Onboarding1';
+import Onboarding2 from './screens/onboarding/Onboarding2';
+import Onboarding3 from './screens/onboarding/Onboarding3';
+import Onboarding4 from './screens/onboarding/Onboarding4';
 import WelcomeScreen2 from './screens/onboarding/WelcomeScreen2';
 
 const App = () => {
   return(
-    <Onboarding1 />
+    <Onboarding4 />
   )
 }
 
